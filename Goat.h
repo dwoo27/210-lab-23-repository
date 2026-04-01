af78ff2b2bf654c4c2e937f0dfc50e347ce58840
dwoo27 <dwooed27@gmail.com>
2026-03-31 20:36:48 -0700

    Update Goat.h and ignore Visual Studio files

--- Goat.h
+++ Goat.h
@@ -15,6 +15,7 @@ public:
     // write three more constructors
     Goat(string n)                  { name = n; age = 0; color = "";  }
     Goat(string n, int a)           { name = n; age = a; color = ""; }
+    Goat(string n, int a, string c) { name = n; age = a; color = c; }
 
     // setters and getters
     void set_name(string n)         { name = n; };
